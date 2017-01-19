f9be5509d769d295078655d81fccad09ba4371c3
Kartik Gola <Kartik Gola>
2017-01-19 23:35:52 +0530

    Removed config. Added env.

--- app.js
+++ app.js
@@ -25,7 +25,7 @@ app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser(config.cookieSecret));
+app.use(cookieParser('Gangadhar is shaktiman'));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(require('express-session')({
   name : 'pMtwitter',
